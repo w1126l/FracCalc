d6a130856b982962fcaec9f6bd9370a6f245b1c1
Winnie Liang <19liangy49@stu.smuhsd.org>
2018-12-19 00:02:30 -0800

    12/19/18 12:02AM

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,3 +1,8 @@
+/* This class contains methods that make up a fractional calculator. The calculator can accept fractional inputs and will return the answer as a mixed number.
+ * @author Winnie Liang
+ * @version 12/18/2018
+ */
+
 package fracCalc;
 
 import java.util.*;
@@ -17,7 +22,7 @@ public class FracCalc {
     	}		
     	userInput.close();
     		
-    }
+    } // This method accepts the question and quits the program once the user says so.
 
     public static String produceAnswer(String input) { 
         
@@ -29,17 +34,63 @@ public class FracCalc {
     	
     	return math(firstOp, operator, secondOp);
     	
-    }
+    } // This method produces the answer to the problem given from the main method.
+    
+    public static int[] splitFrac(String input) {
+    	
+    	int[] all = {0, 0, 1};
+    	
+    	if (input.contains("_")) {
+    		String[] sepWhole = input.split("_");
+    		all[0] = Integer.parseInt(sepWhole[0]);
+    		input = sepWhole[1];
+    	}
+    	
+    	if (input.contains("/")) {
+    		String[] sepFrac = input.split("/");
+    		all[1] = Integer.parseInt(sepFrac[0]);
+    		all[2] = Integer.parseInt(sepFrac[1]);
+    	} else {
+    		all[0] = Integer.parseInt(input);
+    	}
+    	
+    	return toImproperFrac(all);
+    	
+    } // This method splits the fraction given from produceAnswer into an array.
+    
+    public static int[] toImproperFrac(int[] operand) {
+
+    	int whole = operand[0];
+    	int num = operand[1];
+    	int den = operand[2];
+    	
+    	int[] frac = {0, 0};
+    	
+    	if (whole < 0) {
+    		whole = whole * -1;
+    		frac[0] = -1 * ((whole * den) + num);
+    	} else {
+    		frac[0] = ((whole * den) + num);
+    	}
+    	
+    	frac[1] = den;
+    	
+    	return frac;
+    	
+	} // This method returns the mixed number given in splitFrac into an improper fraction.
     
     public static String math(int[] firstOp, String operator, int[] secondOp) {
     	
+    	// Separates the array inputs into clearer names.
     	int num1 = firstOp[0];
     	int den1 = firstOp[1];
     	int num2 = secondOp[0];
     	int den2 = secondOp[1]; 
     	
+    	// Initializes an array to be used later to hold the numerator and denominator after it goes through their given math functions.
     	int[] answer = {0, 0};
     	
+    	// Determines what operation user wants and does it.
     	if (operator.equals("+")) {
        		
     		answer[0] = (num1 * den2) + (num2 * den1);
@@ -62,62 +113,35 @@ public class FracCalc {
         	
     	}
     	
+    	// Declares the numerator and denominator in to the array above.
     	int numerator = answer[0];
     	int denominator = answer[1];
     	
-    	//return numerator + "/" + denominator;
-    	
+    	// Makes the answer given into a mixed number.
     	int[] mixedFrac = toMixedNum(numerator, denominator);
     	int wholeMixed = mixedFrac[0];
     	int numeratorMixed = mixedFrac[1];
     	int denominatorMixed = mixedFrac[2];
     	
-    	return wholeMixed + "_" + numeratorMixed + "/" + denominatorMixed;
+    	// Initializes a variable to return as the answer.
+    	String finalAnswer = null;
     	
-    }
-    
-    public static int[] splitFrac(String input) {
-    	
-    	int[] all = {0, 0, 1};
-    	
-    	if (input.contains("_")) {
-    		String[] sepWhole = input.split("_");
-    		all[0] = Integer.parseInt(sepWhole[0]);
-    		input = sepWhole[1];
-    	}
-    	
-    	if (input.contains("/")) {
-    		String[] sepFrac = input.split("/");
-    		all[1] = Integer.parseInt(sepFrac[0]);
-    		all[2] = Integer.parseInt(sepFrac[1]);
+    	// Determines how to release the answer if there is no whole number or just a fraction.
+    	if (numeratorMixed != 0) {
+    		
+    		if (wholeMixed == 0) {
+    			finalAnswer = numeratorMixed + "/" + denominatorMixed;
+    		} else {
+    			finalAnswer = wholeMixed + "_" + numeratorMixed + "/" + denominatorMixed;
+    		}
+    		
     	} else {
-    		all[0] = Integer.parseInt(input);
+    		finalAnswer = wholeMixed + "";
     	}
     	
-    	return toImproperFrac(all);
+    	return finalAnswer;
     	
-    }
-    
-    public static int[] toImproperFrac(int[] operand) {
-
-    	int whole = operand[0];
-    	int num = operand[1];
-    	int den = operand[2];
-    	
-    	int[] frac = {0, 0};
-    	
-    	if (whole < 0) {
-    		whole = whole * -1;
-    		frac[0] = -1 * ((whole * den) + num);
-    	} else {
-    		frac[0] = ((whole * den) + num);
-    	}
-    	
-    	frac[1] = den;
-    	
-    	return frac;
-    	
-	} 
+    } // This method returns the answer to the problem given from produceAnswer.
    
     public static int[] toMixedNum(int numerator, int denominator) {
 		
@@ -127,94 +151,48 @@ public class FracCalc {
 
     	int simpNum = numerator / gcf;
     	int simpDen = denominator / gcf;
-    	 
-    	fraction[0] = simpNum / simpDen;
-    	fraction[1] = abs
     	
-    	/*if (simpNum < 0) {
-    		
-    		int[] simpNeg = new int[3];
-    		
-    		simpNum = simpNum * -1;
-    		simpNeg[0] = (simpNum - simpDen);
-    		simpNeg[1] = (simpNum % simpDen);
-    		simpNeg[2] = (simpDen);
-    		
-    		fraction[0] = simpNeg[0] * -1;
-    		fraction[1] = simpNeg[1];
-    		fraction[2] = simpNeg[2];
-    		
-    	} else if (simpNum > 0) {
-    		
-    		int[] simpNeg = new int[3];
-
-    		simpNeg[0] = (simpNum - simpDen);
-    		simpNeg[1] = (simpNum % simpDen);
-    		simpNeg[2] = (simpDen);
-    		
-    		fraction[0] = simpNeg[0];
-    		fraction[1] = simpNeg[1];
-    		fraction[2] = simpNeg[2];
-    		
-    	} else {
-    		
-    		fraction[0] = numerator;
-    		
-    	}*/
+    	fraction[0] = simpNum / simpDen;
+    	fraction[1] = absValue(simpNum) % simpDen;
+    	fraction[2] = absValue(simpDen);
     	
     	return fraction;
     	
-    }
+    } // This method takes an improper fraction from math and makes it into a mixed number.
     
     public static int max(int number1, int number2) {
 		
     	if (number1 > number2) {
-			
     		return (number1);
-    		
 		} else {
-			
 			return (number2);
-			
 		}
 		
-	} 
+	} // Returns maximum of two values.
     
     public static int min(int number1, int number2) {
 		
     	if (number1 > number2) {
-    		
 			return (number2);
-			
 		} else if (number1 < number2) {
-			
 			return (number1);
-			
 		} else {
-			
 			return (number1); 
-			
 		}
     	
-	} 
+	} // Returns minimum of two values
 
     public static boolean isDivisibleBy(double dividend, double divisor) {
 		
-    	if (divisor == 0) {
-			
+    	if (divisor == 0) {	
     		throw new IllegalArgumentException("Number cannot be divided by zero.");
-    		
 		} else if (dividend % divisor == 0) { 
-			
 			return true;
-			
 		} else {
-			
 			return false;
-			
 		}
     	
-    }
+    } // Tests to see if one number is divisible by another
     
     public static int gcf(int number1, int number2) {
 		
@@ -223,18 +201,23 @@ public class FracCalc {
 		int i = lg;
 		
 		while (i > 1) {
-			
 			if (isDivisibleBy(sm, i) == true && isDivisibleBy(lg, i) == true) {
 				return i;
-			
-			} 
-			
+			}
 			i--;
-			
 		}
-		
 		return 1;
 	
-    }
-
+    } // Finds the greatest common factor.
+    
+    public static int absValue(int number) {
+		
+    	if (number >= 0) {
+			return (number);
+		} else {
+			return (number * -1);
+		}
+    	
+    } // Finds the absolute value.
+    
 }
